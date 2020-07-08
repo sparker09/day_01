e8b4ba9455e19d5e73642cda49a84e9b65a64e8b
sparker09 <sparker@student.wethinkcode.co.za>
2020-07-08 13:13:42 +0200

    count files corrrected

--- count_files.sh
+++ count_files.sh
@@ -1,38 +1,9 @@
-#!/bin/bash
+find $PWD \( -type f -or -type d \) -name ".*" -prune -o -print | wc -l
 
-LOCATION=$1
-FILECOUNT=0
-DIRCOUNT=0
 
-if [ "$#" -lt "1" ]
-then
-    echo "Usage: ./count_files.sh <directory>"
-    exit 0
-fi
 
-#DIRS=$(find $LOCATION -type d)
-#FILES=$(find $LOCATION -type f)
 
-#for d in $DIRS
-#do
-#   DIRCOUNT=$[$DIRCOUNT+1]
-#done
 
-#for f in $FILES
-#do
-#   FILECOUNT=$[$FILECOUNT+1]
-#done
 
-for item in $LOCATION
-do
-if [ -f "$item" ]
-    then
-         FILECOUNT=$[$FILECOUNT+1]
-    elif [ -d "$item" ]
-        then
-         DIRCOUNT=$[$DIRCOUNT+1]
-fi
-done
 
-echo "File count: " $FILECOUNT
-echo "Directory count: " $DIRCOUNT
+
